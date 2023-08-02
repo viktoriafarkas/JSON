57d70d9dba2694bf3ccdabae9ab87a534f92dae2
viktoriafarkas <115174931+viktoriafarkas@users.noreply.github.com>
2023-08-02 11:52:47 +0200

    Update data.js

--- data.js
+++ data.js
@@ -1,92 +1,92 @@
 const food = [
-    { 
-      id: 1, 
-      name: "Szupersztár", 
-      description: "Legragacsosabb hambink",
-      allergies: ["Tojás mentes", "Hal mentes", "Mogyoró mentes"],
-      ingredients: ["Marhahús pogácsa", "Buci", "Sztár öntet", "Hagyma", "Paradicsom", "Saláta", "Lilakáposzta chutney", "Bacon"],
-      price: 4500,
-      available: "igen",
-      img: "img/hambi-1.jpg"
-    },
-    { 
-      id: 2, 
-      name: "Szarvasgombás Vegán Delight", 
-      description: "Friss zöldségekből készült vegán hamburger",
-      allergies: ["Vegán", "Glutén Mentes", "Szója mentes"],
-      ingredients: ["Húsmentes pogácsa", "Szarvasgomba", "Gluténmentes bucifalatok", "Avokádókrém", "Kukorica", "Uborka", "Saláta", "Vöröshagyma", "Paradicsom"],
-      price: 7200,
-      available: "igen",
-      img: "img/hambi-2.jpg"
-    },
-    { 
-      id: 3, 
-      name: "Mediterrán Gyönyör", 
-      description: "Friss és ízletes görög stílusú hamburger",
-      allergies: ["Glutén Mentes", "Tej mentes"],
-      ingredients: ["Marhahús pogácsa", "Fűszeres olívaolajos bucik", "Feta sajt", "Uborka", "Paradicsom", "Kalamata olívabogyó", "Lilahagyma", "Tzatziki szósz"],
-      price: 5500,
-      available: "nem",
-      img: "img/hambi-3.jpg"
-    },
-    { 
-      id: 4, 
-      name: "Csirkefanatikus", 
-      description: "Csirkemániásoknak tervezett hamburgerspecialitás",
-      allergies: ["Tojás mentes", "Glutén Mentes"],
-      ingredients: ["Csirkemell pogácsa", "Buci", "Jalapeno-joghurt szósz", "Jégsaláta", "Paradicsom", "Kukorica", "Bacon", "Cheddar sajt"],
-      price: 4500,
-      available: "igen",
-      img: "img/hambi-4.jpg"
-    },
-    { 
-      id: 5, 
-      name: "Szaftos Sertésszerelem", 
-      description: "Mézes-mustáros sertéspogácsa",
-      allergies: ["Tojás mentes", "Mogyoró mentes"],
-      ingredients: ["Sertéspogácsa mézes-mustáros glazúrral", "Buci", "Sajtkrém", "Kukorica", "Káposzta", "Szezámmagos lilahagyma", "Jalapeno", "Bacon"],
-      price: 4700,
-      available: "igen",
-      img: "img/hambi-5.jpg"
-    },
-    { 
-      id: 6, 
-      name: "Zöldséges Álom", 
-      description: "Zöldségimádóknak a tökéletes választás",
-      allergies: ["Vegán", "Szója mentes", "Vegetáriánus"],
-      ingredients: ["Vegetáriánus pogácsa", "Vegán bucik", "Hummusz", "Sárgarépa", "Uborka", "Káposzta", "Lilahagyma", "Tahini szósz"],
-      price: 3200,
-      available: "igen",
-      img: "img/hambi-6.jpg"
-    },
-    { 
-      id: 7, 
-      name: "Dupla Szerelmek", 
-      description: "Az ízek táncával elkészített gyros hamburger",
-      allergies: ["Glutén Mentes", "Tej mentes"],
-      ingredients: ["Dupla Marhahús pogácsa", "Buci", "Lilahagyma chutney", "Uborka", "Paradicsom", "Sült paprika", "Ezersziget szósz"],
-      price: 3500,
-      available: "nem",
-      img: "img/hambi-7.jpg"
-    },
-    { 
-      id: 8, 
-      name: "Bajor Mámor", 
-      description: "A bajor konyha esszenciája egy hamburgerben",
-      allergies: ["Tojás mentes", "Tej mentes"],
-      ingredients: ["Sertéspogácsa", "Pretzel bucik", "Mustáros tejfölkrém", "Sauerkraut", "Lilahagyma", "Tükörtojás", "Jalapeno", "Sajtkrém"],
-      price: 3800,
-      available: "nem",
-      img: "img/hambi-8.jpg"
-    },
-    { 
-      id: 9, 
-      name: "Mákos Metró", 
-      description: "Különleges kínai hamburger mákos bucival",
-      allergies: ["Glutén Mentes", "Mogyoró mentes"],
-      ingredients: ["Marhahús pogácsa", "Mákos bucik", "Szójaszószos marhapörkölt", "Káposzta", "Uborka", "Cheddar sajt", "Szezámmagos lilahagyma", "Mákos-mézes szósz"],
-      price: 4500,
-      available: "igen",
-      img: "img/hambi-9.jpg"
-    },
-  ];
+  { 
+    "id": 1, 
+    "name": "Szupersztár", 
+    "description": "Legragacsosabb hambink",
+    "allergies": ["Tojás mentes", "Hal mentes", "Mogyoró mentes"],
+    "ingredients": ["Marhahús pogácsa", "Buci", "Sztár öntet", "Hagyma", "Paradicsom", "Saláta", "Lilakáposzta chutney", "Bacon"],
+    "price": 4500,
+    "available": "igen",
+    "img": "img/hambi-1.jpg"
+  },
+  { 
+    "id": 2, 
+    "name": "Szarvasgombás Vegán Delight", 
+    "description": "Friss zöldségekből készült vegán hamburger",
+    "allergies": ["Vegán", "Glutén Mentes", "Szója mentes"],
+    "ingredients": ["Húsmentes pogácsa", "Szarvasgomba", "Gluténmentes bucifalatok", "Avokádókrém", "Kukorica", "Uborka", "Saláta", "Vöröshagyma", "Paradicsom"],
+    "price": 7200,
+    "available": "igen",
+    "img": "img/hambi-2.jpg"
+  },
+  { 
+    "id": 3, 
+    "name": "Mediterrán Gyönyör", 
+    "description": "Friss és ízletes görög stílusú hamburger",
+    "allergies": ["Glutén Mentes", "Tej mentes"],
+    "ingredients": ["Marhahús pogácsa", "Fűszeres olívaolajos bucik", "Feta sajt", "Uborka", "Paradicsom", "Kalamata olívabogyó", "Lilahagyma", "Tzatziki szósz"],
+    "price": 5500,
+    "available": "nem",
+    "img": "img/hambi-3.jpg"
+  },
+  { 
+    "id": 4, 
+    "name": "Csirkefanatikus", 
+    "description": "Csirkemániásoknak tervezett hamburgerspecialitás",
+    "allergies": ["Tojás mentes", "Glutén Mentes"],
+    "ingredients": ["Csirkemell pogácsa", "Buci", "Jalapeno-joghurt szósz", "Jégsaláta", "Paradicsom", "Kukorica", "Bacon", "Cheddar sajt"],
+    "price": 4500,
+    "available": "igen",
+    "img": "img/hambi-4.jpg"
+  },
+  { 
+    "id": 5, 
+    "name": "Szaftos Sertésszerelem", 
+    "description": "Mézes-mustáros sertéspogácsa",
+    "allergies": ["Tojás mentes", "Mogyoró mentes"],
+    "ingredients": ["Sertéspogácsa mézes-mustáros glazúrral", "Buci", "Sajtkrém", "Kukorica", "Káposzta", "Szezámmagos lilahagyma", "Jalapeno", "Bacon"],
+    "price": 4700,
+    "available": "igen",
+    "img": "img/hambi-5.jpg"
+  },
+  { 
+    "id": 6, 
+    "name": "Zöldséges Álom", 
+    "description": "Zöldségimádóknak a tökéletes választás",
+    "allergies": ["Vegán", "Szója mentes", "Vegetáriánus"],
+    "ingredients": ["Vegetáriánus pogácsa", "Vegán bucik", "Hummusz", "Sárgarépa", "Uborka", "Káposzta", "Lilahagyma", "Tahini szósz"],
+    "price": 3200,
+    "available": "igen",
+    "img": "img/hambi-6.jpg"
+  },
+  { 
+    "id": 7, 
+    "name": "Dupla Szerelmek", 
+    "description": "Az ízek táncával elkészített gyros hamburger",
+    "allergies": ["Glutén Mentes", "Tej mentes"],
+    "ingredients": ["Dupla Marhahús pogácsa", "Buci", "Lilahagyma chutney", "Uborka", "Paradicsom", "Sült paprika", "Ezersziget szósz"],
+    "price": 3500,
+    "available": "nem",
+    "img": "img/hambi-7.jpg"
+  },
+  { 
+    "id": 8, 
+    "name": "Bajor Mámor", 
+    "description": "A bajor konyha esszenciája egy hamburgerben",
+    "allergies": ["Tojás mentes", "Tej mentes"],
+    "ingredients": ["Sertéspogácsa", "Pretzel bucik", "Mustáros tejfölkrém", "Sauerkraut", "Lilahagyma", "Tükörtojás", "Jalapeno", "Sajtkrém"],
+    "price": 3800,
+    "available": "nem",
+    "img": "img/hambi-8.jpg"
+  },
+  { 
+    "id": 9, 
+    "name": "Mákos Metró", 
+    "description": "Különleges kínai hamburger mákos bucival",
+    "allergies": ["Glutén Mentes", "Mogyoró mentes"],
+    "ingredients": ["Marhahús pogácsa", "Mákos bucik", "Szójaszószos marhapörkölt", "Káposzta", "Uborka", "Cheddar sajt", "Szezámmagos lilahagyma", "Mákos-mézes szósz"],
+    "price": 4500,
+    "available": "igen",
+    "img": "img/hambi-9.jpg"
+  },
+];
